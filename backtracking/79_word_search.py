94390eb042316114d67c9f8a8d88c8e6af6811a9
DoDat-12 <dubstep.0509dtd@gmail.com>
2024-11-07 08:57:55 +0700

    071124

--- backtracking/79_word_search.py
+++ backtracking/79_word_search.py
@@ -5,42 +5,60 @@ class Solution(object):
         :type word: str
         :rtype: bool
         """
-        len_x = len(board[0])
-        len_y = len(board)
-        len_word = len(word)
-        res = []
-
-        def is_cand(prev_x, prev_y, next_x, next_y, checked):
-            if [next_x, next_y] in checked:
+        def backtrack(i, j, k):
+            if k == len(word):
+                return True
+            if i < 0 or i >= len(board) or j < 0 or j >= len(board[0]) or board[i][j] != word[k]:
                 return False
-            else:
-                if prev_x == -1 and prev_y == -1:
+            
+            temp = board[i][j]
+            board[i][j] = ''
+            
+            if backtrack(i+1, j, k+1) or backtrack(i-1, j, k+1) or backtrack(i, j+1, k+1) or backtrack(i, j-1, k+1):
+                return True
+            
+            board[i][j] = temp
+            return False
+        
+        for i in range(len(board)):
+            for j in range(len(board[0])):
+                if backtrack(i, j, 0):
                     return True
-                elif next_x < 0 or next_y < 0:
-                    return False
-                elif ((next_x - prev_x == 1 or next_x - prev_x == -1) and next_y - prev_y == 0) or \
-                    ((next_y - prev_y == 1 or next_y - prev_y == -1) and next_x - prev_x == 0):
-                    return True
-                else:
-                    return False
+        return False
 
-        def backtracking(prev_x, prev_y, curr_len, checked, curr_word):
-            for i in range(0, len_y):
-                for j in range(0, len_x):
-                    if is_cand(prev_x, prev_y, i, j, checked):
-                        checked.append([i, j])
-                        curr_word += board[i][j]
-                        curr_len += 1
-                        if curr_len == len_word:
-                            if word == curr_word:
-                                res.append(curr_word)
-                        else:
-                            backtracking(i, j, curr_len, checked, curr_word)
-                        checked.pop()
-                        curr_word = curr_word[:-1]
-                        curr_len -= 1
+        # res = [False]
+        # len_x = len(board) # num_row
+        # len_y = len(board[0]) # num_col
+        # len_word = len(word)
+        # checked = [[False] * len_y for _ in range(len_x)]
 
-        backtracking(-1, -1, 0, [], "")
-        return len(res) > 0
+        # def is_cand(prev_x, prev_y, next_x, next_y):
+        #     if prev_x == -1 and prev_y == -1:
+        #         return True
+        #     elif checked[next_x][next_y]:
+        #         return False
+        #     elif ((next_x - prev_x == 1 or next_x - prev_x == -1) and next_y - prev_y == 0) or \
+        #         ((next_y - prev_y == 1 or next_y - prev_y == -1) and next_x - prev_x == 0):
+        #         return True
+        #     else:
+        #         return False
 
-# TIME LIMIT EXCEEDED
+        # def backtracking(prev_x, prev_y, curr_len, curr_word):
+        #     for i in range(0, len_x):
+        #         for j in range(0, len_y):
+        #             if is_cand(prev_x, prev_y, i, j):
+        #                 checked[i][j] = True
+        #                 curr_word += board[i][j]
+        #                 curr_len += 1
+        #                 if curr_len == len_word:
+        #                     if word == curr_word:
+        #                         res[0] = True
+        #                         return
+        #                 else:
+        #                     backtracking(i, j, curr_len, curr_word)
+        #                 checked[i][j] = False
+        #                 curr_word = curr_word[:-1]
+        #                 curr_len -= 1
+
+        # backtracking(-1, -1, curr_len=0, curr_word="")
+        # return res[0]
---
+++ test/test.py
@@ -0,0 +1,6 @@
+# row = [False] * 4
+# checked = [row] * 5
+# print(checked)
+
+check = [[False] * 4] *5
+print(check)
